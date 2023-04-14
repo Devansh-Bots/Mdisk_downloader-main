bc729c2450e4b62b64848041e4611b9236c91a68
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-14 11:26:50 +0530

    Update and rename config.json to config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+
+import os
+ID = os.environ.get("API_ID", None) 
+HASH = os.environ.get("API_HASH", None) 
+TOKEN = os.environ.get("BOT_TOKEN", None) 
+BAN = os.environ.get("BAN","")
+STRING = os.environ.get("STRING" ,"") 
+TMEP_CHAT = os.environ.get("TMEP_CHAT","")
+OWNER= os.environ.get("OWNER","")
+SUDO=os.environ.get("SUDO","")
+TARGET = os.environ.get("TARGET","")
+LINK = os.environ.get("LINK","")
